0b4e0595df0ee21413c38661aa29a3396ece451d
Christa <Christa09@users.noreply.github.com>
2020-06-01 14:01:46 -0700

    Add project files.

---
+++ YarbroCatering/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YarbroughsCatering.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
